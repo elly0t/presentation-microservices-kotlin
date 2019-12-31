17f46412b1b29ba6665fc58a703889a7e94efbd7
elly0t <elliotwong@oursky.com>
2019-12-31 17:21:13 +0800

    WIP

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { Route, Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
-import App from "./screens/Home";
+import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import NewProductPage from "./screens/NewProduct";
-import ShoppingCart from "./screens/ShoppingCart";
-import Login from "./screens/Login";
 import ForgotPassword from "./screens/ForgotPassword";
-import Register from "./screens/Register";
+import App from "./screens/Home";
+import Login from "./screens/Login";
 import ManageProduct from "./screens/ManageProduct";
+import NewProductPage from "./screens/NewProduct";
+import Register from "./screens/Register";
+import ShoppingCart from "./screens/ShoppingCart";
 
-const history = createBrowserHistory();
+function ECommerce() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Route path="/" exact={true} component={App} />
+      <Route path="/new" exact={true} component={NewProductPage} />
+      <Route path="/cart" exact={true} component={ShoppingCart} />
+      <Route path="/login" exact={true} component={Login} />
+      <Route path="/register" exact={true} component={Register} />
+      <Route path="/forgot" exact={true} component={ForgotPassword} />
+      <Route path="/manage" exact={true} component={ManageProduct} />
+    </BrowserRouter>
+  );
+}
 
-export default (
-  <Router history={history}>
-    <Navbar />
-    <Route path="/" exact={true} component={App} />
-    <Route path="/new" exact={true} component={NewProductPage} />
-    <Route path="/cart" exact={true} component={ShoppingCart} />
-    <Route path="/login" exact={true} component={Login} />
-    <Route path="/register" exact={true} component={Register} />
-    <Route path="/forgot" exact={true} component={ForgotPassword} />
-    <Route path="/manage" exact={true} component={ManageProduct} />
-  </Router>
-);
+export default ECommerce;
